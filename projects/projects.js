2ba6f878cbede2b3f98977dff7360df708da1a2f
akartik80 <akartik80@gmail.com>
2016-04-02 18:04:26 +0530

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -3,5 +3,11 @@ var projects = {
 		"image":"images/1.png",
 		"team":"hitn team",
 		"link":"https://github.com/HackInTheNorth/hackinthenorth.github.io"
+	},
+	
+	"FlashCharge":{
+		"image":"",
+		"team":"Kartik Arora, Vaibhav Jain, Yovan Sethi",
+		"link":"https://github.com/https://github.com/akartik80/flashcharge"
 	}
 }
